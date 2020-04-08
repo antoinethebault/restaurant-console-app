f03cd4d96603f3e588a8359a5fbee1358c0ce22e
antoinethebault <antoine.thebau@gmail.com>
2020-04-08 14:35:52 +0200

    first commit

---
+++ src/main/java/dev/AppSpringXML.java
@@ -0,0 +1,29 @@
+package dev;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import dev.ihm.Menu;
+
+public class AppSpringXML {
+
+	public static void main(String[] args) {
+		
+		// Création du contexte Spring
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext ("application-config-fichier.xml");
+		          
+		// récupération du bean Menu
+		Menu menu = context.getBean(Menu.class); 
+		
+		menu.afficher();
+		          
+		// fermeture du Scanner
+		context.getBean(Scanner.class).close(); 
+		
+		// fermeture du contexte Spring
+		context.close();
+
+	}
+
+}
---
+++ src/main/java/dev/config/AppConfig.java
@@ -0,0 +1,11 @@
+package dev.config;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+//Le stéréotype @Configuration précise que cette classe servira de configuration.
+@Configuration
+public class AppConfig {
+	
+	
+}
