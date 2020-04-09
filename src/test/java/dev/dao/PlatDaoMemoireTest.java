56af7004a2bb8f7c2de5787d1a969cfa4b47cb61
antoinethebault <antoine.thebau@gmail.com>
2020-04-09 15:37:11 +0200

    TP3 - test unitaire minimaliste

---
+++ src/test/java/dev/dao/PlatDaoMemoireTest.java
@@ -0,0 +1,38 @@
+package dev.dao;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import dev.entite.Plat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlatDaoMemoireTest {
+	private PlatDaoMemoire platDaoMemoire;
+
+	@BeforeEach
+	void setUp() {
+		this.platDaoMemoire = new PlatDaoMemoire();
+	}
+
+	@Test
+	void testListerPlatsVideALInitialisation() {
+		List<Plat> listePlatsVide = new ArrayList<>();
+		assertThat(platDaoMemoire.listerPlats()).isEqualTo(listePlatsVide);
+	}
+
+	@Test
+	void testAjouterPlatCasPassants() {
+		List<Plat> listePlats = new ArrayList<>();
+		listePlats.add(new Plat("Plat 1", 1000));
+		listePlats.add(new Plat("Plat 2", 1200));
+		
+		platDaoMemoire.ajouterPlat("Plat 1", 1000);
+		platDaoMemoire.ajouterPlat("Plat 2", 1200);
+		
+		assertThat(platDaoMemoire.listerPlats()).isEqualTo(listePlats);
+		
+	}
+}
